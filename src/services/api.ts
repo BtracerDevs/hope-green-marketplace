34815915a3730e0f0ad45340a994a504a4daa061
Gutemberg Cruz <gute.tec@gmail.com>
2026-04-13 20:34:12 -0300

    change api endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://hope-green-api.vercel.app/'
+  baseURL: 'https://hope-green-api-virid.vercel.app/'
 });
 
 export { api };
